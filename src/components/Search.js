bda7bc6ad65289211eba669916c2466721fdd766
Francisco Sainz <pacoswilliams@gmail.com>
2019-02-08 21:24:33 -0600

    added partial search functionality, will require design changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 
 const Search = props => {
+  let { updateSearch, searchData } = props;
+  console.log(searchData);
+  let results;
+  if (searchData) {
+    results = searchData.map(station => {
+      return (
+        <li key={station.uid}>
+          <h5>{station.station.name}</h5>
+        </li>
+      );
+    });
+  }
   return (
     <div className="search">
-      <input placeholder="Search for a city (Coming Soon)" />
+      <input
+        onChange={e => updateSearch(e.target.value)}
+        placeholder="Search for a city"
+      />
+      <div className="results">
+        <ul>{results}</ul>
+      </div>
     </div>
   );
 };
